99840f055a590b6db04e97b0aa0e16f599e47bfb
LuisMayo <luismayovalbuena@outlook.es>
2020-07-12 23:36:39 +0200

    Divide project into two folders
    One for the auth server and the page to update the URL, another for the function that actually syncs the twitter header with said URL

---
+++ update-header-firebase/functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+functions.pubsub.schedule('every 24 hours').onRun((ctx) => {
+
+});
+// functions.https.onRequest()

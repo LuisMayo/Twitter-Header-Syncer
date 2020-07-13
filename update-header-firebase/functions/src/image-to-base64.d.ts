7806396b02e13c209ed7839118ab931de06029ac
LuisMayo <luismayovalbuena@outlook.es>
2020-07-13 23:45:06 +0200

    Fixed things

--- update-header-firebase/functions/src/image-to-base64.d.ts
+++
@@ -1,3 +0,0 @@
-// declare module 'image-to-base64' {
-//     function imageToBase64(urlOrImage: string): Promise<string>;
-// }
--- update-header-firebase/functions/src/index.ts
+++ update-header-firebase/functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as Twitter from 'twitter';
-import { imageToBase64 } from 'image-to-base64';
+const imageToBase64 = require('image-to-base64');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -15,7 +15,7 @@ import { imageToBase64 } from 'image-to-base64';
 admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
-functions.pubsub.schedule('every 24 hours').onRun((ctx) => {
+exports.scheduledUpdate = functions.pubsub.schedule('every 24 hours').onRun((ctx) => {
   db.collection('twusers').get()
     .then((snapshot) => {
       snapshot.forEach((doc) => {
@@ -32,6 +32,6 @@ function UpdateBanner(doc: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFires
     access_token_key: doc.get('userToken'),
     access_token_secret: doc.get('userTokenSecret')
   });
-  imageToBase64(doc.get('url')).then(string => client.post('account/update_profile_banner', { banner: string }))
-    .catch(e => console.error(e));
+  imageToBase64(doc.get('url')).then((string: string) => client.post('account/update_profile_banner', { banner: string }))
+    .catch((e: Error) => console.error(e));
 }
